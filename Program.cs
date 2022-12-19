ed92c2fac43e34ee85f7e7778029b152e0cfe2b6
ro1-nc <rohan.angne@ncircletech.com>
2022-12-19 18:59:48 +0530

    adding files

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class Program
+    {
+        
+        public static void print_array<T>(T[] result)
+        {
+            foreach (var i in result)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        static void Main(string[] args)
+        {
+            string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+            int[] myNum = { 10, 20, 30, 40 };
+
+            print_array(cars);
+            print_array(myNum);
+
+            Console.ReadKey();
+        }
+
+    }
+}
